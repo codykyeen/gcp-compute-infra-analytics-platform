777112668fa62f620518000bcf8696c039bf93b9
Cody White <codykyeen@gmail.com>
2026-05-25 16:12:04 +0200

    Update latest_instances.sql
    File Tidy Up

--- dbt/resource_models/models/staging/latest_instances.sql
+++ dbt/resource_models/models/staging/latest_instances.sql
@@ -5,6 +5,6 @@ FROM (
                PARTITION BY instance_id
                ORDER BY ingested_at DESC
            ) AS rn
-    FROM `***-prod****.stl_resource_log.instance_data_import`
+    FROM `<gcp_project>.stl_resource_log.instance_data_import`
 )
 WHERE rn = 1
