d83164a27242622fcf07e38cbedb859dd3f7ba44
Cody White <codykyeen@gmail.com>
2026-05-25 16:12:21 +0200

    Update latest_snapshots.sql
    File Tidy Up

--- dbt/resource_models/models/staging/latest_snapshots.sql
+++ dbt/resource_models/models/staging/latest_snapshots.sql
@@ -5,6 +5,6 @@ FROM (
                PARTITION BY snapshot_id
                ORDER BY ingested_at DESC
            ) AS rn
-    FROM `***-prod****.stl_resource_log.snapshot_data_import`
+    FROM `<gcp_project>.stl_resource_log.snapshot_data_import`
 )
 WHERE rn = 1
