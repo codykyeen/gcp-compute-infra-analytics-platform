aa33b5d52d5d0c71cd656f8909df41aad758ab17
Cody White <codykyeen@gmail.com>
2026-05-25 16:11:44 +0200

    Update latest_disks.sql
    File Tidy Up

--- dbt/resource_models/models/staging/latest_disks.sql
+++ dbt/resource_models/models/staging/latest_disks.sql
@@ -5,6 +5,6 @@ FROM (
                PARTITION BY disk_id
                ORDER BY ingested_at DESC
            ) AS rn
-    FROM `***-prod****.stl_resource_log.disk_data_import`
+    FROM `<gcp_project>.stl_resource_log.disk_data_import`
 )
 WHERE rn = 1
